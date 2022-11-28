5a370ed768e4c7fa359bcf4d4c776980bea4a8b3
Syngon <naotivecriatividadepraisso@gmail.com>
2022-11-28 03:01:05 -0300

    project created, need to add all deps xD

--- config/dev.exs
+++ config/dev.exs
@@ -10,6 +10,7 @@ config :aloevera, Aloevera.Repo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
+# Just to commit
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
